e44c7bf87cf1ea5a9dfc08deae6a3e5d5fe1600f
Thi <anhthix7c@gmail.com>
2023-08-22 01:28:21 +0700

    version-12

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 export default function Home() {
   const [data, setData] = useState();
+  const [text, setText] = useState("");
   const url = "/api/test";
   const test = async () => {
     const res = await axios.post(url);
     setData(res.data?.username);
   };
-
   useEffect(() => {
     test();
-  }, [url]);
-
+  });
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
