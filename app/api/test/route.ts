05fb5a49f654cb7fa9592ebdc06925e78fadce17
Thi <anhthix7c@gmail.com>
2023-08-21 23:04:43 +0700

    version-6

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModels";
-
+connect();
 export async function GET() {
   try {
     return NextResponse.json({ message: "hello world" }, { status: 200 });
